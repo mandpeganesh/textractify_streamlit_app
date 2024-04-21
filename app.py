d7de06c55eeb8d0e38f36aa0340af882f409f8c8
GNM <100714789+mandpeganesh@users.noreply.github.com>
2024-04-21 17:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def image_to_text(image_bytes):
     with open('config.json') as config_file:
         config_data = json.load(config_file)
 
-     aws_secret_key = st.secrets["aws_secret_key"]
+    aws_secret_key = st.secrets["aws_secret_key"]
     aws_access_key = st.secrets["aws_access_key"]
 
     # Initialize AWS Textract client
